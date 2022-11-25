d49a51a398d356b72fc405c00ef74d5cd6fa6004
CristianPicares <103083629+CristianPicares@users.noreply.github.com>
2022-11-25 18:37:34 -0300

    Modificacion Administrador

--- eva3/administrador/models.py
+++ eva3/administrador/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class TecnicoMaster(models.Model):
+    usuarioTecnico = models.CharField(primary_key=True, max_length=20)
+    nombreTecnico = models.CharField(max_length=20)
+
+class Usuario(models.Model):
+    usuarioUsuario = models.CharField(primary_key=True, max_length=20)
+    nombreUsuario = models.CharField(max_length=20)
